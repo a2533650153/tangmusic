35a75632238a8f8c49eef1a7bf08b38eb504a1e3
a2533650153 <2533650153@qq.com>
2023-10-11 14:53:12 +0800

    first commit

---
+++ src/main/java/com/tang/music/controller/DefaultController.java
@@ -0,0 +1,16 @@
+package com.tang.music.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class DefaultController {
+
+    @GetMapping
+    public String saveHello(){
+        return "欢迎使用音乐播放器";
+    }
+}
---
+++ src/test/java/com/tang/music/MusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tang.music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
